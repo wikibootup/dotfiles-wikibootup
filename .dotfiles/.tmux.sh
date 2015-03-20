7e863ac2d8eef81f3d541d60f1244ba8d3e9b462
wikibootup <wikibootup@gmail.com>
2015-03-20 19:23:45 +0900

    update tmux setting : shortcuts for using session

--- .dotfiles/.tmux.sh
+++ .dotfiles/.tmux.sh
@@ -7,11 +7,13 @@ alias ct='tmux kill-pane -a'
 # usage : for 50% of horizontal split, $ th 50
 alias 'th'='tmux split-window -v -p'
 alias 'tv'='tmux split-window -h -p'
-alias 'td'='tmux detach'
-alias 'tas'='tmux attach-session -t'
 alias 'tp'='tmux show-buffer'
 alias 'tlb'='tmux list-buffers'
 
+# session
+alias 'tn'='tmux new -s'
+alias 'td'='tmux detach'
+alias 'ta'='tmux attach-session -t'
 # http://osquestions.com/unix-linux/61719/changing-tmux-bash-profile-behavior
 # when tmux start, i can test something
 # called inside tmux session, do tmux things

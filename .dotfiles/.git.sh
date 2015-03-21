e38d06f043ce663b1f76d52bae905a788d2db9e6
wikibootup <wikibootup@gmail.com>
2015-03-22 06:15:52 +0900

    update .git.sh : stash alias, comments

--- .dotfiles/.git.sh
+++ .dotfiles/.git.sh
@@ -6,8 +6,13 @@ if [ -f /usr/local/etc/bash_completion.d/git-completion.bash ]; then
   . /usr/local/etc/bash_completion.d/git-completion.bash
 fi
 
-# git alias
+# Git alias
 alias gadd='git add'
 alias gm='git commit -m'
 alias gd='git diff'
 alias gs='git status'
+
+# Cancel previous load stash
+alias guna='!git stash show -p | git apply -R'
+# More recommend to use followed alias if you set 'git autocomplete'
+# git config --global alias.stash-unapply '!git stash show -p | git apply -R'

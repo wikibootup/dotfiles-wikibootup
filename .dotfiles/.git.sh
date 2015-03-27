035cbef5ba00d8609b5f2c30d566eb34871a9a71
wikibootup <wikibootup@gmail.com>
2015-03-27 18:17:51 +0900

    update dotfiles : apply to show current branch in prompt

--- .dotfiles/.git.sh
+++ .dotfiles/.git.sh
@@ -3,8 +3,12 @@ eval "$(hub alias -s)"
 
 # git autocomplete(also contain hub)
 if [ -f /usr/local/etc/bash_completion.d/git-completion.bash ]; then
-  . /usr/local/etc/bash_completion.d/git-completion.bash
+    . /usr/local/etc/bash_completion.d/git-completion.bash
 fi
+
+# for showing git branch in prompt
+# https://coderwall.com/p/fz0e0g/show-current-git-branch-name-in-bash-ps1
+source /Library/Developer/CommandLineTools/usr/share/git-core/git-prompt.sh
 
 # Git alias
 alias gadd='git add'

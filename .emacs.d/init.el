31c0d83315efdc43aa5c5696819ee4a4c8fee6fb
wikibootup <wikibootup@gmail.com>
2015-08-22 11:42:54 +0900

    add package manager and aggressive indent

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,19 +1,32 @@
 ;; Load path
-(add-to-list 'load-path "~/.emacs.d/")
+(add-to-list 'load-path "~/.emacs.d/lisp")
+
 
 ;; Working directory
 (cd "~/inProgress/sicp/")
+
+
+;; To use MELPA repo
+;; It is built in (bundled) emacs 24
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
+
 
 ;; MIT Scheme
 (setq scheme-program-name "/usr/local/bin/scheme")
 (require 'xscheme)
 
-;; Auto indent
-;; http://emacswiki.org/emacs/AutoIndentation
-(defun set-newline-and-indent ()
-  (local-set-key (kbd "RET") 'newline-and-indent))
-(add-hook 'lisp-mode-hook 'set-newline-and-indent)
-(define-key global-map (kbd "RET") 'newline-and-indent)
+
+;; aggressive-indent-mode ( Auto indent )
+(add-hook 'emacs-lisp-mode-hook #'aggressive-indent-mode)
+(add-hook 'css-mode-hook #'aggressive-indent-mode)
+(global-aggressive-indent-mode 1)
+(add-to-list 'aggressive-indent-excluded-modes 'html-mode)
 
 
 ;; for left side line numbers
@@ -32,7 +45,7 @@
 ;  ;; If there is more than one, they won't work right.
 ; '(gud-gdb-command-name "gdb --annotate=1")
 ; '(large-file-warning-threshold nil)
-; '(quack-programs (quote ("s" "bigloo" "csi" "csi -hygienic" "gosh" "gracket" "gsi" "gsi ~~/syntax-case.scm -" "guile" "kawa" "mit-scheme" "mzscheme" "racket" "racket -il typed/racket" "rs" "scheme" "scheme48" "scsh" "sisc" "stklos" "sxi"))))
+; '(quack-programs (quote ("xscheme" "bigloo" "csi" "csi -hygienic" "gosh" "gracket" "gsi" "gsi ~~/syntax-case.scm -" "guile" "kawa" "mit-scheme" "mzscheme" "racket" "racket -il typed/racket" "rs" "s" "scheme" "scheme48" "scsh" "sisc" "stklos" "sxi"))))
 ;(custom-set-faces
 ;  ;; custom-set-faces was added by Custom.
 ;  ;; If you edit it by hand, you could mess it up, so be careful.

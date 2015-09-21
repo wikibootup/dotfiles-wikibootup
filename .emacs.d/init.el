83ed0d2d39311f0e34fbe4f764dbcda9b5e370c2
wikibootup <wikibootup@gmail.com>
2015-09-21 12:09:36 +0900

    add basic c++ mode : emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -64,6 +64,16 @@
 (setq linum-format "%d ")
 
 
+;; C++
+(require 'cc-mode)
+(setq-default c-basic-offset 4 c-default-style "linux")
+(setq-default tab-width 4 indent-tabs-mode t)
+(define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
+;;(require 'autopair)
+;;(autopair-global-mode 1)
+;;(setq autopair-autowrap t)
+
+
 ;; Scheme <- deactivated because for MIT Scheme
 ;(require 'quack)
 ;(custom-set-variables

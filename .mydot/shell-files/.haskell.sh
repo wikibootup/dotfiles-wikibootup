5eb489d7c845747927ad8bce3740c3c9a88e0d17
wikibootup <wikibootup@gmail.com>
2015-11-28 19:34:30 +0900

    add path : haskell ghc

--- .mydot/shell-files/.haskell.sh
+++ .mydot/shell-files/.haskell.sh
@@ -3,3 +3,10 @@ export PATH=~/.cabal/bin:$PATH
 
 # If you have a ~/Library/Haskell/bin folder on OS X:
 export PATH=~/Library/Haskell/bin:$PATH
+
+# ghc for OSX from website, not using brew
+# Add GHC 7.10.2 to the PATH, via https://ghcformacosx.github.io/
+export GHC_DOT_APP="/Applications/ghc-7.10.2.app"
+if [ -d "$GHC_DOT_APP" ]; then
+  export PATH="${HOME}/.local/bin:${HOME}/.cabal/bin:${GHC_DOT_APP}/Contents/bin:${PATH}"
+fi

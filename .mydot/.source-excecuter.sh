c6a50e1c6da57b043fe3f86d6c4b8928615f4c29
wikibootup <wikibootup@gmail.com>
2015-11-28 19:34:19 +0900

    add comment : .source-excecuter.sh

--- .mydot/.source-excecuter.sh
+++ .mydot/.source-excecuter.sh
@@ -1,3 +1,6 @@
+# Stay the depth of directory structures as 1.
+# Because, I don't manage these as a big program, 
+# but just for executing as a independent source files
 sh_extension_files=$(find $HOME/.mydot/shell-files -name "*.sh")
 for file in $sh_extension_files
 do

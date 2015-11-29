54a05ea609e3edfc2c8edc76479a7a982adf0dc1
wikibootup <wikibootup@gmail.com>
2015-11-29 15:14:56 +0900

    change source name : *.sh -> .*

--- .mydot/.source-excecuter.sh
+++ .mydot/.source-excecuter.sh
@@ -1,7 +1,7 @@
 # Stay the depth of directory structures as 1.
 # Because, I don't manage these as a big program, 
 # but just for executing as a independent source files
-sh_extension_files=$(find $HOME/.mydot/shell-files -name "*.sh")
+sh_extension_files=$(find $HOME/.mydot/shell-files -name ".*")
 for file in $sh_extension_files
 do
   . $file

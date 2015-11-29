a562d3b568424c8f0bce77c8cfeb31c11e43a319
wikibootup <wikibootup@gmail.com>
2015-11-29 17:43:02 +0900

    change dir name : mydot -> bash-dot

--- .bash-dot/.source-excecuter.sh
+++ .bash-dot/.source-excecuter.sh
@@ -1,8 +1,8 @@
 # Stay the depth of directory structures as 1.
 # Because, I don't manage these as a big program, 
 # but just for executing as a independent source files
-sh_extension_files=$(find $HOME/.mydot/shell-files -name ".*")
-for file in $sh_extension_files
+dotfiles=$(find $HOME/.bash-dot/dotfiles -name ".*")
+for file in $dotfiles
 do
   . $file
 done

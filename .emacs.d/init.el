4f991e5d4a39480a3bce0b4a3384ca61a79e1931
wikibootup <wikibootup@gmail.com>
2015-11-29 16:29:50 +0900

    add .emaces : haskell indent

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -74,6 +74,19 @@
 ;;(setq autopair-autowrap t)
 
 
+;; Haskell
+(require 'package)
+(add-hook 'haskell-mode-hook 'haskell-indentation-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-archives
+   (quote
+    (("gnu" . "http://elpa.gnu.org/packages/")
+     ("melpa-stable" . "http://stable.melpa.org/packages/")))))
+
 ;; Scheme <- deactivated because for MIT Scheme
 ;(require 'quack)
 ;(custom-set-variables

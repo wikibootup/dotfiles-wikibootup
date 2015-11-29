adab81adbf89e076d2f530b69342ee42cd2f4950
wikibootup <wikibootup@gmail.com>
2015-11-29 17:54:53 +0900

    change path : to /Users/wikibootup

--- .bash-dot/.source-excecuter.sh
+++ .bash-dot/.source-excecuter.sh
@@ -1,7 +1,7 @@
 # Stay the depth of directory structures as 1.
 # Because, I don't manage these as a big program, 
 # but just for executing as a independent source files
-dotfiles=$(find $HOME/.bash-dot/dotfiles -name ".*")
+dotfiles=$(find $HOME/mydot/.bash-dot/dotfiles -name ".*")
 for file in $dotfiles
 do
   . $file

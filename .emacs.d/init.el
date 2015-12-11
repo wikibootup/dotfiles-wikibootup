78ff71e2d0aaafd67852914de574596cde77d774
wikibootup <wikibootup@gmail.com>
2015-12-11 16:19:12 +0900

    re-init init.el : comback to simple emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,38 +1,40 @@
+;; Comback to the simple emacs.
+;; Complex makes complex.
+
 ;; Load path
-(add-to-list 'load-path "~/.emacs.d/lisp")
-(add-to-list 'load-path "~/inProgress/sicp/modules/")
+;;(add-to-list 'load-path "~/.emacs.d/lisp")
+;;(add-to-list 'load-path "~/inProgress/sicp/modules/")
 
 
 ;; Start-up set
 
 
 ;;;; Shortcut
-(defalias 'f 'find-file)
-(defalias 'r 'run-scheme)
-
-;; Window
-(global-set-key (kbd "M-2") 'split-window-vertically)
-(global-set-key (kbd "M-1") 'split-window-horizontally)
-(global-set-key (kbd "M-0") 'delete-window)
-
-(global-set-key (kbd "M-a") 'shrink-window-horizontally)
-(global-set-key (kbd "M-d") 'enlarge-window-horizontally)
-
-;; M-w SHOULD BE CHANGED (IT IS COPY KEY)
-;(global-set-key (kbd "M-w") 'shrink-window)
-(global-set-key (kbd "M-s") 'enlarge-window)
-
-(global-set-key (kbd "M-f") 'find-file)
-
-;; MIT Scheme
-(global-set-key (kbd "M-r") 'run-scheme)
-
-
+;;(defalias 'f 'find-file)
+;;(defalias 'r 'run-scheme)
+;;
+;;;; Window
+;;(global-set-key (kbd "M-2") 'split-window-vertically)
+;;(global-set-key (kbd "M-1") 'split-window-horizontally)
+;;(global-set-key (kbd "M-0") 'delete-window)
+;;
+;;(global-set-key (kbd "M-a") 'shrink-window-horizontally)
+;;(global-set-key (kbd "M-d") 'enlarge-window-horizontally)
+;;
+;;;; M-w SHOULD BE CHANGED (IT IS COPY KEY)
+;;;(global-set-key (kbd "M-w") 'shrink-window)
+;;(global-set-key (kbd "M-s") 'enlarge-window)
+;;
+;;(global-set-key (kbd "M-f") 'find-file)
+;;
+;;;; MIT Scheme
+;;(global-set-key (kbd "M-r") 'run-scheme)
+;;
+;;
 ;; http://www.emacswiki.org/emacs/WindMove
-;; replacement of C-x-o, just Shift-<arrow>
+;; Replacement of C-x-o, just Shift-<arrow>
 (when (fboundp 'windmove-default-keybindings)
   (windmove-default-keybindings))
-
 
 ;; To use MELPA repo
 ;; It is built in (bundled) emacs 24
@@ -44,27 +46,26 @@
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
 
-
 ;; MIT Scheme
 (setq scheme-program-name "/usr/local/bin/scheme")
 (require 'xscheme)
-
-
+;;
+;;
 ;; aggressive-indent-mode ( Auto indent )
-(add-hook 'emacs-lisp-mode-hook #'aggressive-indent-mode)
-(add-hook 'css-mode-hook #'aggressive-indent-mode)
-(global-aggressive-indent-mode 1)
-(add-to-list 'aggressive-indent-excluded-modes 'html-mode)
-
-
+;;(add-hook 'emacs-lisp-mode-hook #'aggressive-indent-mode)
+;;(add-hook 'css-mode-hook #'aggressive-indent-mode)
+;;(global-aggressive-indent-mode 1)
+;;(add-to-list 'aggressive-indent-excluded-modes 'html-mode)
+;;
+;;
 ;; for left side line numbers
 (require 'linum)
 (linum-mode 1)
 (global-linum-mode 1)
 (setq linum-format "%d ")
-
-
-;; C++
+;;
+;;
+;;;; C++
 (require 'cc-mode)
 (setq-default c-basic-offset 4 c-default-style "linux")
 (setq-default tab-width 4 indent-tabs-mode t)
@@ -72,20 +73,20 @@
 ;;(require 'autopair)
 ;;(autopair-global-mode 1)
 ;;(setq autopair-autowrap t)
-
-
-;; Haskell
-(require 'package)
-(add-hook 'haskell-mode-hook 'haskell-indentation-mode)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-archives
-   (quote
-    (("gnu" . "http://elpa.gnu.org/packages/")
-     ("melpa-stable" . "http://stable.melpa.org/packages/")))))
+;;
+;;
+;;;; Haskell
+;;(require 'package)
+;;(add-hook 'haskell-mode-hook 'haskell-indentation-mode)
+;;(custom-set-variables
+;; ;; custom-set-variables was added by Custom.
+;; ;; If you edit it by hand, you could mess it up, so be careful.
+;; ;; Your init file should contain only one such instance.
+;; ;; If there is more than one, they won't work right.
+;; '(package-archives
+;;   (quote
+;;    (("gnu" . "http://elpa.gnu.org/packages/")
+;;     ("melpa-stable" . "http://stable.melpa.org/packages/")))))
 
 ;; Scheme <- deactivated because for MIT Scheme
 ;(require 'quack)

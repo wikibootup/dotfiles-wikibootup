8630022939a2ccb48d2d5e5fba2fcf765cddc8f8
wikibootup <wikibootup@gmail.com>
2016-01-15 15:09:59 +0900

    modify docker configuration

--- .bash-dot/bash-configs/.docker.sh
+++ .bash-dot/bash-configs/.docker.sh
@@ -1,4 +1,5 @@
 # docker
-export DOCKER_HOST=tcp://192.168.59.103:2376
-export DOCKER_CERT_PATH="$HOME/.boot2docker/certs/boot2docker-vm"
+export DOCKER_HOST=tcp://192.168.99.100:2376
+export DOCKER_MACHINE_NAME=default
 export DOCKER_TLS_VERIFY=1
+export DOCKER_CERT_PATH="$HOME/.docker/machine/machines/default"

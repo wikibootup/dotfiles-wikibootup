c069106a51f6a55b49c3538c9b6fc5ee57008469
wikibootup <wikibootup@gmail.com>
2016-02-18 20:20:21 +0900

    add tmux autoenv config : temporary solution

--- .bash-dot/bash-configs/.virtualenv.sh
+++ .bash-dot/bash-configs/.virtualenv.sh
@@ -1,5 +1,13 @@
+# below code did not work when booting TMUX first,
+# Therefore It was added for TMUX, for now.
+if [ -f ".env" ]; then
+  . .env
+fi
+
 if [ -n "$VIRTUAL_ENV" ]; then
   . "$VIRTUAL_ENV/bin/activate"
 #else
 #  workon default
 fi
+
+
